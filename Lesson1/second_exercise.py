b574584abd3af01b1f5b4403ca85906f56ac84d7
Ermolovich Dima <ermolovichdima22@gmail.com>
2021-11-20 21:39:41 +0300

    upgrade_solution

--- Lesson1/second_exercise.py
+++ Lesson1/second_exercise.py
@@ -1,8 +1,8 @@
 a = float(input("Enter a = "))
 b = float(input("Enter b = "))
 c = float(input("Enter c = "))
-right_part = (a ** 2 + b ** 2) / (3 * b - 4)
-left_part = 4 * c ** 5 / 6
+left_part = (a ** 2 + b ** 2) / (3 * b - 4)
+right_part = 4 * c ** 5 / 6
 print(
-    f"Integer part of number = {int(right_part / left_part)}\n"
-    f"Remainder of the division = {int(right_part % left_part)}")
+    f"Integer part of result = {int(left_part / right_part)}\n"
+    f"Remainder of the division = {left_part % right_part}")
