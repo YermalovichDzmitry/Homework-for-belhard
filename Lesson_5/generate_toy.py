bf72877c5acdba5377b4d80ff9f8839467371a85
Ermolovich Dima <ermolovichdima22@gmail.com>
2021-12-14 11:54:45 +0300

    final_import_lab_commit

--- Lesson_5/generate_toy.py
+++ Lesson_5/generate_toy.py
@@ -3,15 +3,24 @@ import random
 
 
 class NewYearToys(Enum):
-    toy_1 = "Red ball"
-    toy_2 = "Purple angel"
+    toy_1 = "Ball"
+    toy_2 = "Angel"
     toy_3 = "Candle"
     toy_4 = "Teddy bear"
     toy_5 = "Transformer"
     toy_6 = "Car"
 
 
-def get_random_toy() -> bool:
+class Color(Enum):
+    color_1 = "Red"
+    color_2 = "Blue"
+    color_3 = "Yellow"
+    color_4 = "Green"
+    color_5 = "Purple"
+
+
+def get_random_toy() -> int:
     toy = random.choice(list(NewYearToys))
-    print(toy.value)
+    color = random.choice(list(Color))
+    print(f"{color.value} {toy.value}")
     return 0
