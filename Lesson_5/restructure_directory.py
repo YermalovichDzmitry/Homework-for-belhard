21c70190e1e1a24e88640bc0e751dc32b6ad497f
Ermolovich Dima <ermolovichdima22@gmail.com>
2022-01-14 20:17:20 +0300

    corrected

--- Lesson_5/restructure_directory.py
+++ Lesson_5/restructure_directory.py
@@ -11,9 +11,9 @@ def restructure_directory(directory: str) -> None:
             old_file_extension = child.suffix
 
             old_path = old_file_name + old_file_extension
-            full_path = re.sub('-', r"\\", old_path)
+            full_path = re.sub('-', r"/", old_path)
             new_file_name = re.search(r"\d+\.txt", full_path).group(0)
-            path_to_new_file = re.search(r"\d{4}\\\d+\\", full_path).group(0)
+            path_to_new_file = re.search(r"\d{4}/\d+/", full_path).group(0)
             Path(path_start + path_to_new_file).mkdir(parents=True, exist_ok=True)
             Path(path_start + path_to_new_file + new_file_name).touch()
 
